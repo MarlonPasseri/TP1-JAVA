8105ea391f4dca8d85ab8ebc5ccdee6660b9eee9
Marlon Passeri <irmaosfenix.napolitano@gmail.com>
2023-10-29 16:55:06 -0300

    aaa

---
+++ src/main/java/org/example/ClassePacote1.java
@@ -0,0 +1,8 @@
+// ClassePacote1.java (no pacote1)
+package pacote1;
+
+public class ClassePacote1 {
+    public void metodoPacote1() {
+        System.out.println("Método da ClassePacote1 no pacote1.");
+    }
+}
---
+++ src/main/java/org/example/ClassePacote2.java
@@ -0,0 +1,8 @@
+// ClassePacote2.java (no pacote2)
+package pacote2;
+
+public class ClassePacote2 {
+    public void metodoPacote2() {
+        System.out.println("Método da ClassePacote2 no pacote2.");
+    }
+}
